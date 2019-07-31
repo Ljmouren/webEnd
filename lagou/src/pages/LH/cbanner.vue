2d5deab3f3cbe41fe5dd1314282d7693e858e0a6
lyh9322 <903037260@qq.com>
2019-07-31 09:26:43 +0800

    刘慧

---
+++ lagou/src/pages/LH/cbanner.vue
@@ -0,0 +1,243 @@
+<template>
+  <div>
+    <div class="block">
+      <el-carousel trigger="click" height="398px" indicator-position='none' >
+        <el-carousel-item v-for="(item,index) in 4" :key="index">
+              <h3 class="small"> 什么值得投 </h3>
+			  <div class="company_banner" v-if='index==0'>
+				<ul class="it_content">
+					<li >
+						<img src="//www.lgstatic.com/thumbnail_240x240/i/image/M00/C3/CB/Cgp3O1jcvUOATXv_AABZ6zpEAKk174.jpg" alt="" />
+						<p>微盟</p>
+						<div class="time">
+							07/02
+							<span class="ic-before">
+							</span>
+							<span>
+							</span>
+						</div>
+					</li>
+					<li>
+						<img src="../../assets/img/company/CgotOVxmWR6AHajrAAAPTaxGj4w370.jpg" alt="" />
+						<p>车好多</p>
+						<div class="time">
+							07/01
+							<span>
+							</span>
+						</div>
+					</li>
+					<li>
+						<img src="../../assets/img/company/CgqKkVf8uQeAOqJ2AAArfl5skXY149.png" alt="" />
+						<p>有鱼金融科技</p>
+						<div class="time">
+							06/30
+							<span>
+							</span>
+						</div>
+					</li>
+					<li>
+						<img src="../../assets/img/company/CgoB5lwHi-2ADLSxAACEBWVZGpw497.png" alt="" />
+						<p>作业帮</p>
+						<div class="time">
+							06/29
+							<span>
+							</span>
+						</div>
+					</li>
+					<li>
+						<img src="../../assets/img/company/CgotOVvycNiAAe8OAABhrmY_47k309.png" alt="" />
+						<p>贝壳</p>
+						<div class="time">
+							06/28
+							<span>
+							</span>
+						</div>
+					</li>
+				</ul>
+    </div>
+		  <div class="company_banner" v-if='index==1'>
+				<ul class="it_content">
+					<li>
+							<img src="../../assets/img/company/Cgp3O1dQFiaAelOPAAF1O_dbrx8557.jpg" alt="" />
+						<p>微盟2</p>
+						<div class="time">
+							07/02
+							<span class="ic-before">
+							</span>
+							<span>
+							</span>
+						</div>
+					</li>
+					<li>
+						<img src="../../assets/img/company/CgotOVxmWR6AHajrAAAPTaxGj4w370.jpg" alt="" />
+						<p>车好多2</p>
+						<div class="time">
+							07/01
+							<span>
+							</span>
+						</div>
+					</li>
+					<li>
+						<img src="../../assets/img/company/CgqKkVf8uQeAOqJ2AAArfl5skXY149.png" alt="" />
+						<p>有鱼金融科技2</p>
+						<div class="time">
+							06/30
+							<span>
+							</span>
+						</div>
+					</li>
+					<li>
+						<img src="../../assets/img/company/CgoB5lwHi-2ADLSxAACEBWVZGpw497.png" alt="" />
+						<p>作业帮2</p>
+						<div class="time">
+							06/29
+							<span>
+							</span>
+						</div>
+					</li>
+					<li>
+						<img src="../../assets/img/company/CgotOVvycNiAAe8OAABhrmY_47k309.png" alt="" />
+						<p>贝壳2</p>
+						<div class="time">
+							06/28
+							<span>
+							</span>
+						</div>
+					</li>
+				</ul>
+    </div>
+	<div class="company_banner" v-if='index==2'>
+				<ul class="it_content">
+					<li>
+							<img src="../../assets/img/company/Cgp3O1dQFiaAelOPAAF1O_dbrx8557.jpg" alt="" />
+						<p>微盟3</p>
+						<div class="time">
+							07/02
+							<span class="ic-before">
+							</span>
+							<span>
+							</span>
+						</div>
+					</li>
+					<li>
+						<img src="../../assets/img/company/CgotOVxmWR6AHajrAAAPTaxGj4w370.jpg" alt="" />
+						<p>车好多3</p>
+						<div class="time">
+							07/01
+							<span>
+							</span>
+						</div>
+					</li>
+					<li>
+						<img src="../../assets/img/company/CgqKkVf8uQeAOqJ2AAArfl5skXY149.png" alt="" />
+						<p>有鱼金融科技3</p>
+						<div class="time">
+							06/30
+							<span>
+							</span>
+						</div>
+					</li>
+					<li>
+						<img src="../../assets/img/company/CgoB5lwHi-2ADLSxAACEBWVZGpw497.png" alt="" />
+						<p>作业帮3</p>
+						<div class="time">
+							06/29
+							<span>
+							</span>
+						</div>
+					</li>
+					<li>
+						<img src="../../assets/img/company/CgotOVvycNiAAe8OAABhrmY_47k309.png" alt="" />
+						<p>贝壳3</p>
+						<div class="time">
+							06/28
+							<span>
+							</span>
+						</div>
+					</li>
+				</ul>
+    </div>
+	<div class="company_banner" v-if='index==3'>
+				<ul class="it_content">
+					<li>
+							<img src="../../assets/img/company/Cgp3O1dQFiaAelOPAAF1O_dbrx8557.jpg" alt="" />
+						<p>微盟4</p>
+						<div class="time">
+							07/02
+							<span class="ic-before">
+							</span>
+							<span>
+							</span>
+						</div>
+					</li>
+					<li>
+						<img src="../../assets/img/company/CgotOVxmWR6AHajrAAAPTaxGj4w370.jpg" alt="" />
+						<p>车好多4</p>
+						<div class="time">
+							07/01
+							<span>
+							</span>
+						</div>
+					</li>
+					<li>
+						<img src="../../assets/img/company/CgqKkVf8uQeAOqJ2AAArfl5skXY149.png" alt="" />
+						<p>有鱼金融科技4</p>
+						<div class="time">
+							06/30
+							<span>
+							</span>
+						</div>
+					</li>
+					<li>
+						<img src="../../assets/img/company/CgoB5lwHi-2ADLSxAACEBWVZGpw497.png" alt="" />
+						<p>作业帮4</p>
+						<div class="time">
+							06/29
+							<span>
+							</span>
+						</div>
+					</li>
+					<li>
+						<img src="../../assets/img/company/CgotOVvycNiAAe8OAABhrmY_47k309.png" alt="" />
+						<p>贝壳4</p>
+						<div class="time">
+							06/28
+							<span>
+							</span>
+						</div>
+					</li>
+				</ul>
+    </div>
+	
+        </el-carousel-item>
+      </el-carousel>
+    </div>
+   
+  </div>
+</template>
+
+<script>
+export default {
+
+
+
+};
+</script>
+
+<style lang="less">
+  .el-carousel__item h3 {
+	color: white;
+	text-align: center;
+	padding: 20px 0 30px;
+	font-size: 30px;
+  }
+
+  .el-carousel__item:nth-child(2n) {
+     background-color: #00b38b;
+  }
+  
+  .el-carousel__item:nth-child(2n+1) {
+     background-color:#00b38b;
+  }
+
+</style>
---
+++ lagou/src/pages/LH/ccontent.vue
@@ -0,0 +1,127 @@
+<template>
+  <div>
+    <div class="c-wrap">
+					<!--筛选-->
+				<ul class="filter">
+						<li><strong>公司地点：</strong>
+							<a href="#" class="active">全国</a>
+							<a href="#">北京</a>
+							<a href="#">上海</a>
+							<a href="#">广州</a>
+							<a href="#">深圳</a>
+							<a href="#">成都</a>
+							<a href="#">南京</a>
+							<a href="#">武汉</a>
+							<a href="#" class="more">更多&nbsp;<i></i></a>
+						</li>
+						<li><strong>融资阶段：</strong>
+							<a href="#" class="active">不限</a>
+							<a href="#">未融资</a>
+							<a href="#">天使轮</a>
+							<a href="#">A轮</a>
+							<a href="#">B轮</a>
+							<a href="#">C轮</a>
+							<a href="#">D轮</a>
+							<a href="#">D轮以上</a>
+						</li>
+						<li><strong>公司规模：</strong>
+							<a href="#" class="active">不限</a>
+							<a href="#">少于15人</a>
+							<a href="#">15-50人</a>
+							<a href="#">50-150人</a>
+							<a href="#">150-500人</a>
+							<a href="#">500-2000人</a>
+							<a href="#">2000人以上</a>
+						</li>
+						<li><strong>行业领域：</strong>
+							<a href="#" class="active">不限</a>
+							<a href="#">移动互联网</a>
+							<a href="#">电商</a>
+							<a href="#">金融</a>
+							<a href="#">企业服务</a>
+							<a href="#">教育</a>
+							<a href="#">文娱</a>
+							<a href="#">游戏</a>
+							<a href="#" class="more">更多&nbsp;<i></i></a>
+						</li>
+					</ul>
+					<!--排序方式-->
+					<ul class="filter">
+						<li><strong>排序方式：</strong>
+							<a href="#" class="active">默认排序</a>
+							<a href="#">职位数量↓</a>
+							<a href="#">面试评价数↓</a>
+							<a href="#">简历处理率↓</a>
+							<span class="check"><i></i>有面试评价</span>
+						</li>
+					</ul>	
+				</div>
+   
+  </div>
+</template>
+
+
+<script>
+export default {};
+</script>
+
+<style lang="less">
+.c-wrap {
+  width: 1200px;
+  max-width: 1200px;
+  margin: 0 auto;
+}
+
+ .filter {
+  background: #fafafa;
+  border: 1px solid #ededed;
+  padding: 5px 15px;
+  margin: 20px 0px;
+}
+.filter li {
+  margin: 5px 0px;
+}
+.filter li a {
+  margin-right:5px;
+  display: inline-block;
+  padding: 4px 8px;
+  color: #555;
+}
+.filter li a.active {
+  color: white;
+  background: #00b38a;
+}
+.filter li .more {
+  float: right;
+  margin: 0 16px;
+  position: relative;
+}
+.content .filter li .more i {
+  position: absolute;
+  top: 9px;
+  right: -3px;
+  font-size: 0;
+  height: 0;
+  width: 0;
+  border-width: 6px 6px 0;
+  border-style: solid dashed;
+  border-color: #00b38a transparent transparent;
+  overflow: hidden;
+}
+ .filter li .check {
+  float: right;
+  margin: 0 16px;
+  position: relative;
+  color: #555;
+}
+ .filter li .check i {
+  display: inline-block;
+  vertical-align: middle;
+  margin: -3px 3px 0;
+  width: 18px;
+  height: 15px;
+  background: url(../../assets/img/company/check_f5603cd.png) no-repeat;
+}
+</style>
+
+
